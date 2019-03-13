19f0b40975407b4f048a33d7918555741a864704
anton1113 <anton11131113@gmail.com>
2019-03-13 15:56:35 +0200

    Initial commit

---
+++ src/main/java/com/arash/flux/LearnFluxApplication.java
@@ -0,0 +1,13 @@
+package com.arash.flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arash/flux/LearnFluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arash.flux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnFluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
